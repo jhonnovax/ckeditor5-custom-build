4fe1a04a34212e27634f52f9350bb33d8668241a
Jhon Nova <jhonfredynova@gmail.com>
2021-08-27 16:56:06 -0500

    Created and added plugin tokens to custom build

--- plugin/tokens.js
+++ plugin/tokens.js
@@ -5,66 +5,64 @@ import Collection from '@ckeditor/ckeditor5-utils/src/collection';
 import TokensIcon from '../plugin/tokens.svg';
 
 class Tokens extends Plugin {
-  init() {
-    const editor = this.editor;
+	init() {
+		const editor = this.editor;
 
-    editor.ui.componentFactory.add('tokens', locale => {
-      // Toolbar button
-      const view = createDropdown(locale);
-      const items = new Collection();
+		editor.ui.componentFactory.add( 'tokens', locale => {
+			// Toolbar button
+			const view = createDropdown( locale );
+			const items = new Collection();
 
-      view.buttonView.set( {
-        icon: TokensIcon,
-        label: 'Insert token',
-        tooltip: true
-      });
+			view.buttonView.set( {
+				icon: TokensIcon,
+				label: 'Insert token',
+				tooltip: true
+			} );
 
-      items.add( {
-        type: 'button',
-        model: new Model({
-          withText: true,
-          label: 'FIRSTNAME',
-          value: 'FIRSTNAME'
-        })
-      });
-      
-      items.add( {
-        type: 'button',
-        model: new Model({
-          withText: true,
-          label: 'LASTNAME',
-          value: 'LASTNAME'
-        })
-      });
+			items.add( {
+				type: 'button',
+				model: new Model( {
+					withText: true,
+					label: 'FIRSTNAME',
+					value: 'FIRSTNAME'
+				} )
+			} );
 
-      items.add( {
-        type: 'button',
-        model: new Model({
-          withText: true,
-          label: 'EMAIL',
-          value: 'EMAIL'
-        })
-      });
-      
-      addListToDropdown(view, items);
+			items.add( {
+				type: 'button',
+				model: new Model( {
+					withText: true,
+					label: 'LASTNAME',
+					value: 'LASTNAME'
+				} )
+			} );
 
-      // Callback executed once the toolbar button is clicked
-      view.on('execute', event => {
-        const tokenName = `#${event.source.value}#`;
+			items.add( {
+				type: 'button',
+				model: new Model( {
+					withText: true,
+					label: 'EMAIL',
+					value: 'EMAIL'
+				} )
+			} );
 
-        editor.model.change(writer => {
-          const spanElement = writer.createElement('span');
-          writer.insertText(tokenName, spanElement);
-          writer.setAttribute('style', 'background: yellow; padding: 10px;', spanElement)
-          editor.model.insertContent(spanElement, editor.model.document.selection);
-        });
+			addListToDropdown( view, items );
 
-      });
+			// Callback executed once the toolbar button is clicked
+			view.on( 'execute', event => {
+				const tokenName = `#${ event.source.value }#`;
 
-      return view;
-    });
-  }
+				editor.model.change( writer => {
+					const spanElement = writer.createElement( 'span' );
+					writer.insertText( tokenName, spanElement );
+					writer.setAttribute( 'style', 'background: yellow; padding: 10px;', spanElement );
+					editor.model.insertContent( spanElement, editor.model.document.selection );
+				} );
+			} );
 
+			return view;
+		} );
+	}
 }
 
 export default Tokens;
