ce5d9fddf3a2843c872ac5ce65d36fc1b0d5da62
Jhon Nova <jhonfredynova@gmail.com>
2021-08-27 14:54:15 -0500

    Added first plugin tokens version

---
+++ plugin/tokens.js
@@ -0,0 +1,9 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+class Tokens extends Plugin {
+    init() {
+        console.log( 'Tokens was initialized' );
+    }
+}
+
+export default Tokens;
